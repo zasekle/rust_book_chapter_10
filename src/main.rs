535480682c9232ba707fdb6275401bcda9650e21
Jeremiah Gleason <quickshiftx@gmail.com>
2023-06-12 15:50:00 -0400

    Did section 10.3. This is the end of Chapter 10.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fmt::Display;
 fn main() {
     generic_data_types();
     traits_defining_shared_behavior();
+    validating_references_with_lifetimes();
 }
 
 fn generic_data_types() {
@@ -215,4 +216,126 @@ fn traits_defining_shared_behavior() {
             }
         }
     }
+}
+
+
+fn validating_references_with_lifetimes() {
+    //Every reference has a lifetime, which the the scope for which that reference is valid.
+    // lifetimes can be annotated as a method of specifying relationships between lifetimes.
+    // I am new to this feature (never seen it in another programming language), and quite frankly
+    // the only value I can see to it is in passing a variable through a function without making
+    // a copy.
+
+    //Essentially the lifetimes are communicated using the lifetime annotation below. This `says`
+    // that there is a relationship between the lifetimes of x, y and the return value.
+    fn largest_int<'a>(x: &'a i32, y: &'a i32) -> &'a i32 {
+        if x > y {
+            &x
+        } else {
+            &y
+        }
+    }
+
+    let int1 = 1;
+    let int2 = 2;
+
+    let res = largest_int(&int1, &int2);
+    println!("result: {res}");
+
+    //Because of the above function definition, all lifetimes of the parameters must be valid for
+    // the compiler to allow res to be used. For example, the below code will not compile.
+    // let int1 = 2;
+    // let res;
+    // {
+    //     let int2 = 1;
+    //     res = largest_int(&int1, &int2);
+    // }
+    // println!("result: {res}");
+
+    //They go through that you cannot return a reference to an object that will be deallocated at
+    // the end of a function. But coming from c++ this is the norm so I am not going to focus too
+    // much on it.
+
+    //structs also require lifetime annotations in order to hold references.
+    struct MyStuff<'a> {
+        my_str: &'a str
+    }
+
+    let my_str = String::from("the string");
+    let stuff = MyStuff{my_str: my_str.as_str()};
+
+    println!("stuff string: {}", stuff.my_str);
+
+    //There are also certain exceptions that happen so frequently the compiler can fill in the
+    // lifetime annotations for me. These exceptions are called `lifetime elision rules`.
+    // There are currently three rules.
+
+    //Rule #1: The compiler assigns a lifetime parameter to each parameter that's a reference.
+    // For example
+    // fn foo<'a>(a: &<'a>i32) { ...
+    //  can be written as
+    // fn foo(a: &i32) { ...
+
+    //Rule #2: If there is exactly one input lifetime parameter, that lifetime is assigned to all
+    // output lifetime parameters.
+    // For example
+    fn single_param(x: &i32) -> &i32 {
+        x
+    }
+
+    println!("single_param: {}", single_param(&int1));
+
+    //Rule #3: If there are multiple input parameters, but one of them is &self or &mut self
+    // (meaning that this is a method) then the lifetime of self is assigned to all output lifetime
+    // parameters.
+
+    struct World {
+        happy: bool,
+        sad: bool,
+    }
+
+    impl World {
+        fn winner(&self, happiness: &i32) -> &bool {
+            if *happiness > 5 {
+                &self.happy
+            } else {
+                &self.sad
+            }
+        }
+    }
+
+    let world = World{happy: true, sad: false};
+
+    let happiness = 10;
+    println!("Happy? {}", world.winner(&happiness));
+
+    //As a side note I didn't realise that Rust had different definitions of `Method` and
+    // `Function`. I have been using them interchangeably. A Method is a function declared on a
+    // struct, enum or trait with the first parameter of self. A function seems to be everything
+    // else starting with the `fn` keyword that is not a method.
+
+    //The lifetime annotation for impl must be used if a reference type is stored inside the
+    // object.
+    impl<'a> MyStuff<'a> {
+        fn sad(&self) -> i32 {
+            3
+        }
+    }
+
+    println!("sad: {}", stuff.sad());
+
+    //`'static` is a special lifetime. It allows for the variable to be directly written into the
+    // programs binary. When &str is used for a string literal, by default it is set to the static
+    // lifetime annotation.
+    let hello_world = "Hello world!";
+    let static_int: &'static i32 = &123; //A manual example of 'static.
+
+    println!("hello_world: {hello_world} static_int: {static_int}");
+
+    //A lifetime can be used with a generic as follows.
+    // fn largest_int<'a, T>(x: &'a i32, y: &'a i32, z: T) -> &'a i32
+    // where
+    //     T: Display,
+    // { ...
+
 }
